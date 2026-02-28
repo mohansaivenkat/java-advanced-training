851e5d539947ad281fdb663da7a20f54c7799ed5
mohansaivenkat <mohansaivenkat2004@gmail.com>
2026-02-28 09:46:04 +0530

    23rd fintech assignment pushed

---
+++ 23-Feb-Assignment-FinTechHibernateJPA/src/main/java/com/fintechhibernateJPA/JpaUtil.java
@@ -0,0 +1,21 @@
+package com.fintechhibernateJPA;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JpaUtil {
+
+	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("fintech-pu");
+
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+	
+	public static void close() {
+		emf.close();
+	}
+	
+
+}
